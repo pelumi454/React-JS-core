ffaadf26631f06421b26edbb19ffb7859acd6d65
pelumi454 <amoochristianah454@gmail.com>
2022-08-29 14:17:56 +0100

    fix: changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
+import './App.css';
 
 function App() {
+  const todayDate = new Date();
+  const timeToday = todayDate.toLocaleTimeString();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Hi! The time is {timeToday}</h4>
     </div>
   );
 }
