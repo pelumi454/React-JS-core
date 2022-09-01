aa2515087627f7a61653bb04e9e3ccb5e3491aa6
pelumi454 <amoochristianah454@gmail.com>
2022-09-01 09:09:57 +0100

    fix: create reusable component in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CommentDetail from "./components/CommentDetail";
+import ApprovalCard from "./components/ApprovalCard";
 
-function App() {
-  const todayDate = new Date();
-  const timeToday = todayDate.toLocaleTimeString();
+if (module.hot) {
+  module.hot.accept();
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <h4>Hi! The time is {timeToday}</h4>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          // avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="I like the subject"
+        // avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="I like the writing"
+        // avatar={faker.image.image()}
+      />
     </div>
   );
-}
+};
 
 export default App;
